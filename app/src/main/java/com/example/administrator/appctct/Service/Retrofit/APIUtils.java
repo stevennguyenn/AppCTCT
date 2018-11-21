f331476e69343720aabce12e73c4ee3396865d4e
steve <1510821@hcmut.edu.vn>
2018-11-22 00:15:13 +0700

    fix file

--- app/src/main/java/com/example/administrator/appctct/Service/Retrofit/APIUtils.java
+++
@@ -1,9 +0,0 @@
-package com.example.administrator.appctct.Service.Retrofit;
-
-public class APIUtils {
-    public static final String baseURL = "http://10.20.79.128/CTCT/";
-
-    public static DataClient getData(){
-        return RetrofitClient.getClient(baseURL).create(DataClient.class);
-    }
-}
